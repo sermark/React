e6d5fa6b4ba3b06b767f606e47496ab9f8b2b3c1
Sergy <win295@gmail.com>
2017-11-29 16:44:13 +0300

    fix

--- static/components/addtodo.js
+++ static/components/addtodo.js
@@ -6,49 +6,43 @@ class AddToDo extends Component {
         super(props);
 
         this.state = {
-            term: '',
-            items: {
-                item : [],
-                date: ''
-            }
+            items: []
         };
     }
 
     onSubmit = (event) => {
-        const today = new Date();
         event.preventDefault();
-        if(this.state.term == '') {
-            return false
+        const newValue = this.input.value;
+
+        if(newValue == '') {
+            return false;
         } else {
+            var newItem = {
+                text: newValue,
+                date: new Date().toLocaleTimeString(),
+            }
+            
+            var newItems = [...this.state.items].push(newItem);
+
             this.setState({
-                term: '',
-                items: {
-                    item: [...this.state.items.item, this.state.term],
-                    date : new Date()
-                }
+                items: newItems
             });
+
         }
-    }
-    onChange = (event) => {
-        this.setState({term: event.target.value})
     }
 
     render() {
         return (
             <div>
                 <form onSubmit={this.onSubmit}>
-                    <input value={this.state.term} onChange = {this.onChange}/>
+                    <input type="text" ref={(input) => this.input = input} />
                     <button type="submit">Add ToDo</button>
                 </form>
-                {/* <List items={this.state.items}/> */}
                 <ul>
                     {
-                   
-                    
-
-                    this.state.items.item.map((item,index) => <li key={index}>
-                        {item.item}
-                        <span>{this.state.items.date.toLocaleTimeString()}</span>
+                    this.state.items.map((item,index) => <li key={index}>
+                        {item.text}
+                        <span>{item.date}</span>
                         <button>Edit</button>
                         <button>Remove</button>
                     </li>)
@@ -59,39 +53,10 @@ class AddToDo extends Component {
     }
 }
 
-var h =  {
-    getTime: function() {
-       var d = new Date();
-       var day = d.getDate();
-       var mon = d.getMonth();
-       var year = d.getFullYear();
-       var sec = d.getSeconds();
-       var dateAll = day + '.' + mon + '.' + year + "-" + sec;
-     
-       return dateAll;
-    }
-  };
-
-
-
-
-const setDate = () => {
-    const todayDate = new Date();
-    const date = todayDate.getDate() + '.' + (todayDate.getMonth() + 1) + '.' + todayDate.getFullYear() + '-' + todayDate.getSeconds(); 
-    return date;
-}   
-
-// const List = props => (
-//     <ul>
-//         {
-//             props.items.map((item,index) => <li key={index}>
-//                 {item}
-//                 <span>{setDate()}</span>
-//                 <button>Edit</button>
-//                 <button>Remove</button>
-//             </li>)
-//         }
-//     </ul>    
-// );
+// const setDate = () => {
+//     const todayDate = new Date();
+//     const date = todayDate.getDate() + '.' + (todayDate.getMonth() + 1) + '.' + todayDate.getFullYear() + '-' + todayDate.getSeconds(); 
+//     return date;
+// }   
 
 export default AddToDo;
