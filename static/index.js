1f4488cd9b6e4ae268afe54d232b96cef106025e
Sergy <s.markovich@unlitsolutions.com>
2017-12-27 15:56:11 +0300

    solve conflict

--- static/index.js
+++ static/index.js
@@ -7,36 +7,11 @@ import configureStore from './store/Store';
 import { addTodo, removeTodo, editTodo, toggleTodo, sortTodo } from './actions';
 import './main.sass';
 
-<<<<<<< HEAD
-import TodoForm from './components/TodoForm';
-import MyEditor from './components/draft';
-import SerchBar from './components/searchBar';
-// import axios from 'axios';
-=======
->>>>>>> redux
 
 import App from './components/App';
 
-<<<<<<< HEAD
-class App extends Component {
-
-    search = searchText => {
-        this.todoForm.searching(searchText);
-    }
-
-    render() {
-        return (
-        <div> 
-            <SerchBar search={this.search}/>
-            <TodoForm ref={(todoForm) => this.todoForm = todoForm}/>
-        </div>
-        );
-    }
-}
-=======
 const dest = document.getElementById('root');
 let store = configureStore();
->>>>>>> redux
 
 render(
   <Provider store={store}>
